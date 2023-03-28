18d3313468c65d81ee0c4748f68f59f3bad1ae5f
Achal Singhal <achals2002@gmail.com>
2023-03-29 00:08:19 +0530

    some icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,37 +1,43 @@
-import { Slot } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import { colours } from '../styles/colours';
 
 export default function HomeLayout() {
   return (
     <>
       <Slot />
       <View style={navbarStyles.bar}>
-        <View style={navbarStyles.icon}>
+        <View style={navbarStyles.iconContainer}>
           <Image
             source={require('../components/common/icons/home.svg')}
-            style={{
-              width: 24,
-              height: 24,
-            }}
+            style={navbarStyles.icon}
           />
         </View>
-        <View style={navbarStyles.icon}></View>
-        <View style={navbarStyles.icon}></View>
-        <View style={navbarStyles.icon}></View>
-        <View style={navbarStyles.icon}></View>
-        {/* <View>
-          <Text>Notifications</Text>
+        <View style={navbarStyles.iconContainer}>
+          <Image
+            source={require('../components/common/icons/notifs.svg')}
+            style={navbarStyles.icon}
+          />
         </View>
-        <View>
-          <Text>Find/Search</Text>
+        <View style={navbarStyles.iconContainer}>
+          <Image
+            source={require('../components/common/icons/search.svg')}
+            style={navbarStyles.icon}
+          />
         </View>
-        <View>
-          <Text>Spotify Shit</Text>
+        <View style={navbarStyles.iconContainer}>
+          <Image
+            source={require('../components/common/icons/barChart.svg')}
+            style={navbarStyles.icon}
+          />
         </View>
-        <View>
-          <Text>Profile</Text>
-        </View> */}
+        <View style={navbarStyles.iconContainer}>
+          <Image
+            source={require('../components/common/icons/person.svg')}
+            style={navbarStyles.icon}
+          />
+        </View>
       </View>
       <StatusBar style="auto" />
     </>
@@ -42,18 +48,22 @@ const navbarStyles = StyleSheet.create({
   bar: {
     flex: 1,
     flexDirection: 'row',
-    backgroundColor: '#bbb090',
+    backgroundColor: colours.BaseA,
     alignItems: 'center',
     justifyContent: 'space-between',
     minHeight: 36,
     maxHeight: 36,
     width: '100%',
   },
-  icon: {
+  iconContainer: {
     flex: 1,
     width: '1/5',
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  icon: {
+    width: 24,
+    height: 24,
   },
 });
--- src/styles/colours.ts
+++ src/styles/colours.ts
@@ -1,5 +1,8 @@
-const enum colours {
-    primary = "#bbb090",
-    secondary = "#CAB891",
-
+export const enum colours {
+    BaseA = "#BBB090",
+    BaseB = "#CAB891",
+    GreenNice = "#70683B",
+    GreenWeird = "#5F6A64",
+    GreenDark = "#2B2C1E",
+    Red = "#81231E"
 }
